ec9b734a589a894b7da6e6343825145cfa250553
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2020-02-25 23:23:00 +0300

    Updates to the programm that can get all the cities. The main trick was to set limit and skip right in the query formation.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,7 +21,7 @@ namespace ConsoleApp1
             var x = task.Result;
             foreach (ParseObject o in x)
             {
-                Console.WriteLine(o["name"]);
+                Console.WriteLine("{0} - {1}", o["name"], (o["country"] as ParseObject)["name"]);
             }
             ;
         }
@@ -30,8 +30,8 @@ namespace ConsoleApp1
         {
             ParseClient.Initialize(new ParseClient.Configuration
             {
-                ApplicationId = "gkkEC0CESNT5L5VVrUi4CydYOiIFVunbKOWT2lPZ",
-                WindowsKey = "JtuBujBqkpefaeXOaDmIjX97uncLya5SfYHs9rw9",
+                ApplicationId = "XW0rXBZxcGJcl3zkoWrh3w6bryBpkXnG36CvzOPV",
+                WindowsKey = "C3O1Mn02ELUtYpKSBUbfUXf2dOh0snBHR8yoIbx6",
                 Server = "https://parseapi.back4app.com/"
             }
             );
@@ -39,27 +39,23 @@ namespace ConsoleApp1
             IEnumerable<ParseObject> cities = null;
             List<ParseObject> listOfCities = new List<ParseObject>();
 
-            ParseQuery<ParseObject> countryQuery = from country in ParseObject.GetQuery("Continentscountriescities_Country")
-                                                   where country.ContainsKey("name")
-                                                   select country;
-
-            ParseQuery<ParseObject> query = from city in ParseObject.GetQuery("Continentscountriescities_City")
-                                            .Include("country.name")
-                                            //join country in countryQuery on city["country"] equals country
-                                            where city.ContainsKey("name")
-                                            select city;
+            ParseQuery<ParseObject> query =
+                    ParseObject.GetQuery("City")
+                    .Include("country");
+                    
             int count = await query.CountAsync();
-            query.Limit(100);
-            cities = await query.FindAsync();
-            listOfCities.AddRange(cities.ToList<ParseObject>());
-            //for (int i =0; i < count;)
-            //{
-            //    query.Skip(i);
-            //    cities = await query.FindAsync();
-            //    listOfCities.AddRange( cities.ToList<ParseObject>());
-            //    i += 1000;
-            //}
-            
+ 
+            int limit = 1000;
+            for (int i = 0; i < count;)
+            {
+                query =
+                    ParseObject.GetQuery("City")
+                    .Include("country").Skip(i).Limit(limit);
+                cities = await query.FindAsync();
+                listOfCities.AddRange(cities.ToList<ParseObject>());
+                i += limit;
+            }
+
             return listOfCities;
         }
     }
